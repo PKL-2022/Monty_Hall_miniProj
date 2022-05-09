cb7a99a990f1cc5c36a6491b7880fc29bb5610c5
jinsuc28 <jinsuc28@gmail.com>
2022-05-09 21:12:53 +0900

    feat: make function
    사장님 화이팅

--- Monty_hall.py
+++ Monty_hall.py
@@ -1,7 +1,28 @@
 import random
 
-dorr = ['a','b','c']
+door = ['a','b','c']
 no_change = 0
 change = 0
 
 trial = 100000
+
+for _ in range(trial):
+    car = random.randint(0,2)
+    player_choice = random.randint(0,2)
+
+    empty_door = []
+
+    for i in range(3):
+        if i!=player_choice and i != car:
+            empty_door.append(door[i])
+
+        com = random.sample(empty_door, 1)[0]
+
+        if player_choice == car:
+            no_change += 1
+
+        empty_door.remove(com)
+
+
+        if not empty_door:
+            change += 1
